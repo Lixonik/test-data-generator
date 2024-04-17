c2ce5ccb7fdcff39398a5c6467e4dfa813efa123
Boris Prokhorov <deimos.75@mail.ru>
2024-04-17 21:14:26 +0300

    refactoring, combined generateMeaningfulString-s functions

--- lib/lib/utils.d.ts
+++ lib/lib/utils.d.ts
@@ -1,5 +1,5 @@
 import { Trie } from './trie/trie';
-import { ClassFields, Nil, NumberOpts, PartNameOpts, StringOpts } from './types';
+import { ClassFields, Language, MeaningfulStringOpts, Nil, NumberOpts, PartNameOpts, StringOpts } from './types';
 /**
  * Fisher-Yates Sorting Algorithm
  * https://www.freecodecamp.org/news/how-to-shuffle-an-array-of-items-using-javascript-or-typescript/
@@ -12,7 +12,7 @@ export declare const isEmpty: (obj: any) => boolean;
 export declare const isNil: (value: unknown) => value is Nil;
 export declare const isDefined: <T>(value: T | Nil) => value is T;
 export declare const mergeInitValueWithPartialOpts: <T extends object>(partialOpts: Partial<T>, initOpts: T) => T;
-export declare class CPartNameOpts {
+export declare class CPartNameOpts implements PartNameOpts {
     gender: PartNameOpts['gender'];
     type: PartNameOpts['type'];
     length: PartNameOpts['length'];
@@ -20,13 +20,19 @@ export declare class CPartNameOpts {
     padej: PartNameOpts['padej'];
     constructor(opts: Partial<ClassFields<CPartNameOpts>>);
 }
-export declare class CStringOpts {
+export declare class CStringOpts implements StringOpts {
     charSet: StringOpts['charSet'];
     length: StringOpts['length'];
     constructor(opts: Partial<ClassFields<CStringOpts>>);
 }
-export declare class CNumberOpts {
-    min: NumberOpts['min'];
-    max: NumberOpts['max'];
+export declare class CNumberOpts implements NumberOpts {
+    min: number;
+    max: number;
     constructor(opts: Partial<ClassFields<CNumberOpts>>);
+}
+export declare class CMeaningfulStringOpts implements MeaningfulStringOpts {
+    length: number;
+    language: Language;
+    separator: string;
+    constructor(opts: Partial<ClassFields<CMeaningfulStringOpts>>);
 }
